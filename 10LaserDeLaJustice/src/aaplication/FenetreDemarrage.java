5639f2f6dfd0d01050539515f103848c5edf6a43
miora <miora.r.rakoto@gmail.com>
2019-02-26 21:42:45 -0500

    Personnage bouge selon les touches choisies. **Modification constructeur personnage**

--- 10LaserDeLaJustice/src/aaplication/FenetreDemarrage.java
+++ 10LaserDeLaJustice/src/aaplication/FenetreDemarrage.java
@@ -30,7 +30,6 @@ public class FenetreDemarrage extends JFrame {
 			public void run() {
 				try {
 					FenetreDemarrage frame = new FenetreDemarrage();
-				//	frame.jeu.requestFocusInWindow();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,7 +42,7 @@ public class FenetreDemarrage extends JFrame {
 	 * Create the frame.
 	 */
 	public FenetreDemarrage() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 661, 509);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
--- 10LaserDeLaJustice/src/options/Options.java
+++ 10LaserDeLaJustice/src/options/Options.java
@@ -7,38 +7,40 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.BufferedOutputStream;
+import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JSpinner;
+import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.border.EmptyBorder;
 
 import utilite.ModeleDonnee;
-import javax.swing.colorchooser.ColorChooserComponentFactory;
-import javax.swing.JList;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
+/**
+ * Cette classe permet a l'utilisateur de modifier les parametres du jeu. 
+ * Ces parametres sont le niveau, la gravite, les touches de clavier et la couleur du rayon 
+ * @author Miora R. Rakoto
+ */
 
 public class Options extends JFrame {
 
 	private JPanel contentPane;
 	private JTextField textField;
-	private String toucheGauche = "Gauche", toucheDroite = "Droite";
+	private int toucheGauche = 37, toucheDroite = 39;
 	private ModeleDonnee modele;
 	private JTable table;
+	private JSpinner snpDif;
+	private JSpinner snpAcc;
 
-	
-
-	/**
-	 * Cette classe permet a l'utilisateur de modifier les parametres du jeu. 
-	 * Ces parametres sont le niveau, la gravite, les touches de clavier et la couleur du rayon 
-	 * @author Miora R. Rakoto
-	 */
-	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -53,83 +55,83 @@ public class Options extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Creation de la fenetre.
 	 */
 	public Options() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 787, 480);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JLabel lblOptJeu = new JLabel("Options de jeu");
 		lblOptJeu.setFont(new Font("Lucida Sans", Font.PLAIN, 50));
 		lblOptJeu.setBounds(233, 47, 394, 64);
 		contentPane.add(lblOptJeu);
-		
+
 		JLabel lblLvlDif = new JLabel("Niveau de difficult\u00E9 :");
 		lblLvlDif.setFont(new Font("Lucida Sans", Font.PLAIN, 18));
 		lblLvlDif.setBounds(30, 132, 199, 30);
 		contentPane.add(lblLvlDif);
-		
+
 		JLabel lblAccG = new JLabel("Acc\u00E9l\u00E9ration gravitationelle :\r\n");
 		lblAccG.setBounds(30, 212, 274, 22);
 		lblAccG.setFont(new Font("Lucida Sans", Font.PLAIN, 18));
 		contentPane.add(lblAccG);
-		
+
 		JLabel lblTouchePourSe = new JLabel("Touches pour se d\u00E9placer :");
 		lblTouchePourSe.setBounds(30, 297, 274, 30);
 		lblTouchePourSe.setFont(new Font("Lucida Sans", Font.PLAIN, 18));
 		contentPane.add(lblTouchePourSe);
-		
+
 		JLabel lblCouleurDuRayon = new JLabel("Couleur du rayon :");
 		lblCouleurDuRayon.setBounds(30, 369, 240, 22);
 		lblCouleurDuRayon.setFont(new Font("Lucida Sans", Font.PLAIN, 18));
 		contentPane.add(lblCouleurDuRayon);
-		
-		JSpinner snpDif = new JSpinner();
+
+		snpDif = new JSpinner();
 		snpDif.setFont(new Font("Lucida Sans", Font.PLAIN, 14));
 		snpDif.setModel(new SpinnerNumberModel(1, 1, 5, 1));
 		snpDif.setBounds(460, 140, 54, 43);
 		contentPane.add(snpDif);
-		
-		JSpinner snpAcc = new JSpinner();
+
+		snpAcc = new JSpinner();
 		snpAcc.setFont(new Font("Lucida Sans", Font.PLAIN, 14));
 		snpAcc.setModel(new SpinnerNumberModel(new Double(9.8), new Double(0), null, new Double(0.1)));
 		snpAcc.setBounds(460, 216, 54, 43);
 		contentPane.add(snpAcc);
-		
-		JLabel lblGaucheTxt = new JLabel("Gauche :");
+
+		JLabel lblGaucheTxt = new JLabel();
 		lblGaucheTxt.setFont(new Font("Lucida Sans", Font.PLAIN, 14));
 		lblGaucheTxt.setBounds(370, 306, 77, 21);
 		contentPane.add(lblGaucheTxt);
-		
+
 		JLabel lblDroiteTxt = new JLabel("Droite :");
 		lblDroiteTxt.setFont(new Font("Lucida Sans", Font.PLAIN, 14));
 		lblDroiteTxt.setBounds(564, 304, 63, 19);
 		contentPane.add(lblDroiteTxt);
-		
-		JButton btnG = new JButton(toucheGauche);
+
+		JButton btnG = new JButton("Gauche");
 		btnG.setBackground(Color.WHITE);
 		btnG.setFont(new Font("Lucida Sans", Font.PLAIN, 12));
 		btnG.setOpaque(true);
-		
+
 		btnG.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				btnG.addKeyListener(new KeyAdapter() {
 					@Override
 					public void keyPressed(KeyEvent e) {
-						toucheGauche = KeyEvent.getKeyText(e.getKeyCode()) ;
-						btnG.setText(toucheGauche );
+						toucheGauche =e.getKeyCode() ;
+						btnG.setText(KeyEvent.getKeyText(e.getKeyCode()));
 					}
 				});
 			}
 		});
 		btnG.setBounds(434, 304, 100, 55);
 		contentPane.add(btnG);
-		
-		JButton btnD = new JButton(toucheDroite);
+
+		JButton btnD = new JButton("Droite");
 		btnD.setFont(new Font("Lucida Sans Typewriter", Font.PLAIN, 12));
 		btnD.setBackground(Color.WHITE);
 		btnD.setBounds(636, 304, 100, 55);
@@ -138,29 +140,52 @@ public class Options extends JFrame {
 				btnD.addKeyListener(new KeyAdapter() {
 					@Override
 					public void keyPressed(KeyEvent e) {
-						toucheDroite = KeyEvent.getKeyText(e.getKeyCode()) ;
-						btnD.setText(toucheDroite );
+						toucheDroite = e.getKeyCode() ;
+						btnD.setText(KeyEvent.getKeyText(e.getKeyCode()));
 					}
 				});
 			}
 		});
 		btnG.setOpaque(true);
 		contentPane.add(btnD);
-		
-	}
-	public String getToucheGauche() {
-		return toucheGauche;
-	}
 
-	public void setToucheGauche(String toucheGauche) {
-		this.toucheGauche = toucheGauche;
+		JButton btnSauvegarder = new JButton("Sauvegarder");
+		btnSauvegarder.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ecritureFichier();
+			}
+		});
+		btnSauvegarder.setBounds(636, 399, 100, 43);
+		contentPane.add(btnSauvegarder);
 	}
+	/**
+	 * Cette methode permet de sauvegarder les options du choisi par l'utilisateur
+	 */
+	private void ecritureFichier(){
+		final String NOM_FICHIER_OPTION = "DonneeOption.d3t";
+		File fichierDeTravail = new File(NOM_FICHIER_OPTION);
 
-	public String getToucheDroite() {
-		return toucheDroite;
-	}
-
-	public void setToucheDroite(String toucheDroite) {
-		this.toucheDroite = toucheDroite;
+		DataOutputStream fluxSortie = null;
+		try {
+			fluxSortie = new DataOutputStream(new BufferedOutputStream(	new FileOutputStream(fichierDeTravail)));
+			fluxSortie.writeInt(Integer.parseInt(snpDif.getValue().toString()));
+			fluxSortie.writeDouble(Double.parseDouble(snpAcc.getValue().toString()));
+			fluxSortie.writeInt(toucheGauche);
+			fluxSortie.writeInt(toucheDroite);
+			JOptionPane.showMessageDialog(null,"Vos modifications ont ete sauvegardees avec succes");
+		} 
+		catch (IOException e) {
+			System.out.println("Erreur lors de l'écriture!");
+			e.printStackTrace();
+		}
+		finally {
+			//on exécutera toujours ceci, erreur ou pas
+			try { 
+				fluxSortie.close();  
+			}
+			catch (IOException e) { 
+				System.out.println("Erreur rencontrée lors de la fermeture!"); 
+			}
+		}//fin finally
 	}
 }
