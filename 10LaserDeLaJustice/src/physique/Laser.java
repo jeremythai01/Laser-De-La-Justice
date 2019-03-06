2cd1e20a3513d3bb116975dfa440b501ad321c39
arnau <arnau@192.168.2.44>
2019-03-06 14:01:00 -0500

    Ajout de commentaires dans laser

--- 10LaserDeLaJustice/src/physique/Laser.java
+++ 10LaserDeLaJustice/src/physique/Laser.java
@@ -21,7 +21,7 @@ import interfaces.Dessinable;
  *
  */
 public class Laser implements Dessinable{
-	private double LONGUEUR= 1.5;
+	private final double LONGUEUR= 1.5;
 	private Vecteur position, vitesse;
 	private double angleTir;
 	private Vecteur accel;
@@ -34,6 +34,13 @@ public class Laser implements Dessinable{
 
 	Random rand = new Random();
 	
+	/**
+	 * Constructeur du laser dont la position, la vitesse ainsi que l'angle de tir sont specifies
+	 * @param position, la position de depart du laser
+	 * @param angleTir, l'angle selon lequel le laser est tire
+	 * @param vitesse, la vitesse du laser
+	 */
+	//auteur Arnaud Lefebvre
 	public Laser(Vecteur position, double angleTir, Vecteur vitesse) {
 		
 		this.position=position;
@@ -46,6 +53,15 @@ public class Laser implements Dessinable{
 
 
 	}
+	/**
+	 * Permet de dessiner le laser selon le contexte graphique en parametre.
+	 * @param g2d contexte graphique
+	 * @param mat matrice de transformation monde-vers-composant
+	 * @param hauteur hauteur du monde reelle
+	 * @param largeur largeur du monde reelle
+	 * 
+	 */
+	//auteur Arnaud Lefebvre
 	public void dessiner(Graphics2D g2d, AffineTransform mat, double hauteur, double largeur) {
 		trace = new Path2D.Double();
 		AffineTransform matLocal = new AffineTransform(mat);
@@ -57,6 +73,11 @@ public class Laser implements Dessinable{
 
 	}
 
+	/**
+	 * Methode qui permet de donner une couleur aleatoire au laser
+	 * @param g2d, le contexte graphique
+	 */
+	//auteur Jeremy Thai
 	private void randomColor(Graphics2D g2d) {
 		float i = (float) 0.5;
 		float r = (rand.nextFloat()/2f) + i;
@@ -68,11 +89,19 @@ public class Laser implements Dessinable{
 	}
 
 
+	/**
+	 * 
+	 * @return
+	 */
+	//auteur Jeremy Thai
 	public double getLigneFinY() {
 		return ligneFinY;
 	}
 
-
+	/**
+	 * 
+	 */
+	//auteur Jeremy Thai
 	public void move() { 
 		ligneFinY -= vitesse.getY();
 		ligneDebutX+=vitesse.getX();
@@ -81,11 +110,20 @@ public class Laser implements Dessinable{
 		//System.out.println("position de laser quand il bouge" + ligneDebutX +" "+ ligneFinY) ;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	//auteur Jeremy Thai
 	public double getLigneDebutX() {
 		return ligneDebutX;
 	}
 
-
+	/**
+	 * 
+	 * @param ligneDebutX
+	 */
+	//auteur Jeremy Thai
 	public void setLigneDebutX(double ligneDebutX) {
 		this.ligneDebutX = ligneDebutX;
 	}
@@ -107,49 +145,101 @@ public class Laser implements Dessinable{
     }
 	 */
 
+	/**
+	 * 
+	 * @return
+	 */
+	//auteur Jeremy Thai
 	public Area getLaserAire(){ // pour detecter lintersection
 		return new Area(new Rectangle2D.Double(position.getX(), ligneFinY,LONGUEUR,position.getY()));
 	}
 
+	/**
+	 * Retourne la longueur du laser
+	 * @return, LONGUEUR, constante de longueur du laser
+	 */
+	//auteur Arnaud Lefebvre
 	public double getLONGUEUR() {
 		return LONGUEUR;
 	}
 
-	public void setLONGUEUR(double lONGUEUR) {
-		LONGUEUR = lONGUEUR;
-	}
 
+	/**
+	 * Retourne la position du laser
+	 * @return, le vecteur de la position du laser
+	 */
+	//auteur Arnaud Lefebvre
 	public Vecteur getPosition() {
 		return new Vecteur (position.getX(),position.getY());
 	}
 
+	/**
+	 * Permet de modifier la position du laser
+	 * @param position, la nouvelle position desiree en vecteur
+	 */
+	//auteur Arnaud Lefebvre
 	public void setPosition(Vecteur position) {
 		this.position = position;
 	}
 
+	/**
+	 * Retourne la vitesse du laser
+	 * @return, vitesse, le vecteur vitesse du laser
+	 */
+	//auteur Arnaud Lefebvre
 	public Vecteur getVitesse() {
 		return vitesse;
 	}
 
+	/**
+	 * Permet de modifier la vitesse du laser
+	 * @param vitesse, la nouvelle vitesse desire
+	 */
+	//auteur Arnaud Lefebvre
 	public void setVitesse(Vecteur vitesse) {
 		this.vitesse = vitesse;
 	}
 
+	/**
+	 * Retourne l'angle du laser
+	 * @return, angleTir, l'angle du laser
+	 */
+	//auteur Arnaud Lefebvre
 	public double getAngleTir() {
 		return angleTir;
 	}
 
+	/**
+	 * Permet de modifier l'angle de tir du laser
+	 * @param angleTir, le nouvel angle de tir desire
+	 */
 	public void setAngleTir(double angleTir) {
 		this.angleTir = angleTir;
 		updaterAngleVitesse(angleTir);
 	}
+	/**
+	 * 
+	 * @return
+	 */
+	//auteur Jeremy Thai
 	public Vecteur getAccel() {
 		return accel;
 	}
 
+	/**
+	 * 
+	 * @param accel
+	 */
+	//auteur Jeremy Thai
 	public void setAccel(Vecteur accel) {
 		this.accel = accel;
 	}
+	/**
+	 * Methode qui permet d'orienter le laser selon son angle
+	 * Cette methode permet de s'assurer que le laser avance bien dans le sens de son angle
+	 * @param angle, l'angle du laser
+	 */
+	//auteur Arnaud Lefebvre
 	public void updaterAngleVitesse(double angle) {
 		double vitesseEnX=0.5*Math.cos(Math.toRadians(angle));
 		double vitesseEnY=0.5*Math.sin(Math.toRadians(angle));
