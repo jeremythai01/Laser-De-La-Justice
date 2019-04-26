ba4b547a2628834f6d24a29db30c7964b0f91c4b
thai <thai@PC-GAMING>
2019-04-25 23:33:30 -0400

    merge

--- 10LaserDeLaJustice/src/interfaces/SceneListener.java
+++ 10LaserDeLaJustice/src/interfaces/SceneListener.java
@@ -6,11 +6,13 @@ import java.util.EventListener;
 import physique.Balle;
 /**
  * Interface qui contient les evenements
- * @author Jeremy Thai 
+ * @author Miora
  */
 public interface SceneListener extends EventListener{
-	
-	public void couleurLaserListener();
+	/**
+	 * Cet ecouteur modifie le temps si celui-ci a ete change
+	 * @param temps : le nouveau temps
+	 */
 	public void changementTempsListener(int temps);
 	public void modeScientifiqueListener(ArrayList<Balle> listeBalles, double hauteurMonde);
 }
